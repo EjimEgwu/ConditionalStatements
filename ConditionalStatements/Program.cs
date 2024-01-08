4d31a1f355a9b5f6a0b6cfe9ac0a42abe0ce42a8
PROGRESSIVE <PROGRESSIVE@DESKTOP-Q2A87JN>
2024-01-08 10:55:44 +0100

    Add project files.

---
+++ ConditionalStatements/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Accepting a string of color and displaying it
+Console.WriteLine("Enter a color");
+string color = Console.ReadLine();
+
+if (color == "red")
+{
+    Console.WriteLine("box #1");
+}
+else if (color == "green")
+{
+    Console.WriteLine("box #2");
+}
+else if (color == "black")
+{
+    Console.WriteLine("box #3");
+}
+else
+{
+    Console.WriteLine("unknown");
+}
